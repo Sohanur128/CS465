f4630d5387574c4466c300c28b47304abc3260bc
Sohanur Rahman <sohanur.rahman@snhu.edu>
2025-05-19 20:31:34 -0500

    testing

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index'); //new added
+var travelRouter = require('./app_server/routes/travel'); //added 
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+
+
+
+// register handlebars partials
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter); //added
+app.use('/travel', travelRouter); //added 
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
