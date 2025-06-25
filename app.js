6014cf39e3773da113dfeb07c1ec483dc84e05ed
Sohanur Rahman <sohanur.rahman@snhu.edu>
2025-06-25 11:25:34 -0500

    Maybe Final

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require('dotenv').config(); //step 8, page 191 [bring the variables defined in the file into our memory space]
+
 const cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// Wire in our authentication module: Module 7
+var passport = require('passport');
+require('./app_api/config/passport');
+
 
 var indexRouter = require('./app_server/routes/index');
 var travelRouter = require('./app_server/routes/travel');
@@ -25,13 +31,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize()); // Module 7
 
 // Enable CORS for Angular (localhost:4200)
-app.use(cors({
-  origin: 'http://localhost:4200',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+//app.use(cors({
+//  origin: 'http://localhost:4200',
+//  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//  allowedHeaders: ['Content-Type', 'Authorization']
+//}));
+
+// Enable CORS : Module 7
+app.use('/api', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
 
 
 // Routes
@@ -52,5 +68,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+// Catch unauthorized error and create 401 : Module 7
+app.use((err, req, res, next) => { 
+    if (err.name === 'UnauthorizedError') { 
+        res 
+            .status(401) 
+            .json({ "message": err.name + ": " + err.message }); 
+    } 
+});
+
+
 
 module.exports = app;
